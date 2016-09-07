270b5fc2d1eee17feaa99e7c01cb739cf94e77bf
root <root@localhost.localdomain>
2016-09-07 10:21:54 +0000

    Add mroute

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,14 @@
 class profile::base {
 
   #the base profile should include component modules that will be on all nodes
+ class { '::ntp':
+  servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ]
 
+  network::mroute { 'eth0':
+  routes => {
+    '192.168.10.0/24' => '192.168.2.1',
+    '10.0.0.0/8'     => '192.168.5.1',
+    '80.81.82.0/16'  => 'eth0',
+  }
+}
 }
